d76b6d3f8ca5e12d7345197631b4073ac124c5fa
unknown <alu2015111338@CA-LAB101-64031.uniritter.local>
2015-05-22 21:57:14 -0300

    Estrutura inicial

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Define some constants
+ */
+define("DS", DIRECTORY_SEPARATOR);
+define("ROOT", realpath(dirname(__DIR__)) . DS);
+define("VENDORDIR", ROOT . "vendor" . DS);
+define("TEMPLATEDIR", ROOT . "public/templates" . DS);
+define("CACHETEMPLATEDIR", ROOT . "public/templates/cache" . DS);
+
+/**
+ * Include autoload file
+ */
+if (file_exists(VENDORDIR . "autoload.php")) {
+    require_once VENDORDIR . "autoload.php";
+} else {
+    die("<pre>Execute '/c/xampp/php/php composer.phar install' na raiz do projeto</pre>");
+}
+
+$loader = new Twig_Loader_Filesystem(TEMPLATEDIR);
+$twig = new Twig_Environment($loader, array(
+    'cache' => CACHETEMPLATEDIR,
+));
+
+echo $twig->render('index.html', array('name' => 'João'));
